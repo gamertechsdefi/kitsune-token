e7a142cb8b1abfe4527e41687b4ab71fb00b1eab
Gamer <gamertechs.defi@gmail.com>
2024-02-13 15:03:34 +0100

    updated

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
-import './Main.css'
+import "./Main.css";
 
 function Main() {
   return (
@@ -22,7 +22,7 @@ function Main() {
               animation: "fadeInOut 4s ease-in-out infinite",
             }}
           >
-            THE SPIRIT KEEPERS
+            THE MOST POWERFUL FOX
           </h1>
           <p
             className="text-lg text-white p-4 md:p-8 lg:p-12"
@@ -31,7 +31,11 @@ function Main() {
               letterSpacing: "1px",
             }}
           >
-           The Nine Tailed Fox is 
+            Welcome to the realm of Kitsune Token, where innovation meets
+            tradition in the world of decentralized finance (DeFi). Inspired by
+            the legendary Japanese fox spirit known for its cunning intellect
+            and adaptability, Kitsune Token is here to revolutionize the DeFi
+            landscape.
           </p>
         </div>
       </div>
